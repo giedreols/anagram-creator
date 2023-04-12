132d961f41061ec04f1682e9e1757b12347f2245
Vilius Kaunas <vilius.kaunas@visma.com>
2023-04-12 17:06:21 +0300

    Adds missing import

--- AnagramSolver/BusinessLogic/Program.cs
+++ AnagramSolver/BusinessLogic/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // this file should be in AnagramSolver.Cli
+
+using System.Text;
 using BusinessLogic.AnagramActions;
 using BusinessLogic.InputWordActions;
 using Cli;
